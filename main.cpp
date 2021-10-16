8a7c3727ff77c50b6d7b4b7e475f256788cb0445
iKarans <68602829+iKarans@users.noreply.github.com>
2021-10-16 19:07:31 +0100

    added check winner method

--- main.cpp
+++ main.cpp
@@ -16,7 +16,31 @@ class Game
                 }
                 std::cout << "-------" << std::endl;   
             };
+        bool check_winner()
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+                    {
+                        return true;
+                    };        
+                };
+                for (int i = 0; i < 3; i++)
+                {
+                    if (board[i][0] == board[i][1] && board[i][1] == board[i][2])
+                    {
+                        return true;
+                    };
+                };
+
+                if ((board[0][0] == board[1][1] && board[1][1] == board[2][2]) || (board[2][0] == board[1][1] && board[1][1] == board[0][2])) 
+                {
+                    return true;
+                };  
+                return false;
+            }
         void game()
+            while()
             {
                 print_board();
                 int choice;

5a652711ef2e36f701f896b520c9dcff4c662f38
iKarans <68602829+iKarans@users.noreply.github.com>
2021-10-16 18:56:19 +0100

    added print board method

--- main.cpp
+++ main.cpp
@@ -5,4 +5,14 @@ class Game
 {
     public:
         std::string board[3][3] = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
-}
+        bool playerOneTurn = true;
+        void print_board()
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    std::cout << "-------" << std::endl;
+                    std::cout << "|" << board[i][0] << "|" << board[i][1] << "|" << board[i][2] << "|" <<std::endl;
+                }
+                std::cout << "-------" << std::endl;   
+            };
+};

6012ad91cfde9eaa5b7e3d981a6b2e6a25c176f5
iKarans <68602829+iKarans@users.noreply.github.com>
2021-10-16 19:02:14 +0100

    added game method for single round

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ class Game
     public:
         std::string board[3][3] = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
         bool playerOneTurn = true;
+        int rounds = 0;
         void print_board()
             {
                 for (int i = 0; i < 3; i++)
@@ -14,5 +15,22 @@ class Game
                     std::cout << "|" << board[i][0] << "|" << board[i][1] << "|" << board[i][2] << "|" <<std::endl;
                 }
                 std::cout << "-------" << std::endl;   
+            };
+        void game()
+            {
+                print_board();
+                int choice;
+                std::cout << "Choose a Number: ";
+                std::cin >> choice;
+                if(playerOneTurn)
+                {
+                    board[(choice - 1) / 3][(choice - 1) % 3] = "X";
+                } 
+                else 
+                {
+                    board[(choice - 1) / 3][(choice - 1) % 3] = "O";
+                }
+                playerOneTurn = !playerOneTurn;
+                rounds++;         
             };
 };
